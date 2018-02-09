0daf497544f7da050554de64e62891843b43fe1c
kawax <kawaxbiz@gmail.com>
2018-02-09 16:17:35 +0900

    wordfence

---
+++ web/app/wordfence-waf.php
@@ -0,0 +1,8 @@
+<?php
+// Before removing this file, please verify the PHP ini setting `auto_prepend_file` does not point to this.
+
+if (file_exists(__DIR__ . '/plugins/wordfence/waf/bootstrap.php')) {
+    define("WFWAF_LOG_PATH", __DIR__ . '/wflogs/');
+    include_once __DIR__ . '/plugins/wordfence/waf/bootstrap.php';
+}
+?>
