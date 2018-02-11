91be80eb484eeb6ea1c12927800a520fb9f7c117
kawax <kawaxbiz@gmail.com>
2018-02-11 12:56:56 +0900

    share

--- web/app/themes/wp-update/resources/views/partials/header.blade.php
+++ web/app/themes/wp-update/resources/views/partials/header.blade.php
@@ -8,5 +8,28 @@
         {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']) !!}
       @endif
     </nav>
+    <div class="mt-4">
+      <a href="https://twitter.com/share"
+         class="twitter-share-button"
+         data-hashtags="WordPress"
+         data-show-count="false">Tweet</a>
+      <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+      <a href="http://b.hatena.ne.jp/entry/s/wordpress-update.info/"
+         class="hatena-bookmark-button"
+         data-hatena-bookmark-layout="basic-label-counter"
+         data-hatena-bookmark-lang="ja"
+         title="このエントリーをはてなブックマークに追加">
+        <img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+             alt="このエントリーをはてなブックマークに追加"
+             width="20"
+             height="20"
+             style="border: none;"/>
+      </a>
+      <script type="text/javascript"
+              src="https://b.st-hatena.com/js/bookmark_button.js"
+              charset="utf-8"
+              async="async"></script>
+    </div>
   </div>
 </header>
