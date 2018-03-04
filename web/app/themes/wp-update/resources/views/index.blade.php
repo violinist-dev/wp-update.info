bebba696f3ae6335ad723adbface175edfaeb9bc
kawax <kawaxbiz@gmail.com>
2018-03-04 12:03:40 +0900

    index

--- web/app/themes/wp-update/resources/views/index.blade.php
+++ web/app/themes/wp-update/resources/views/index.blade.php
@@ -1,18 +1,21 @@
 @extends('layouts.app')
 
 @section('content')
-  @include('partials.page-header')
+  <div class="container">
 
-  @if (!have_posts())
-    <div class="alert alert-warning">
-      {{ __('Sorry, no results were found.', 'sage') }}
-    </div>
-    {!! get_search_form(false) !!}
-  @endif
+    @include('partials.page-header')
 
-  @while (have_posts()) @php(the_post())
-  @include('partials.content-'.get_post_type())
-  @endwhile
+    @if (!have_posts())
+      <div class="alert alert-warning">
+        {{ __('Sorry, no results were found.', 'sage') }}
+      </div>
+      {!! get_search_form(false) !!}
+    @endif
 
-  {!! get_the_posts_navigation() !!}
+    @while (have_posts()) @php(the_post())
+    @include('partials.content-'.get_post_type())
+    @endwhile
+
+    {!! get_the_posts_navigation() !!}
+  </div>
 @endsection
