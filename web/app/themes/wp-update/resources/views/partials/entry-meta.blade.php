51890387f440461a92aeb4f13638f001646a36ec
kawax <kawaxbiz@gmail.com>
2018-03-17 10:59:37 +0900

    delete category

--- web/app/themes/wp-update/resources/views/partials/entry-meta.blade.php
+++ web/app/themes/wp-update/resources/views/partials/entry-meta.blade.php
@@ -3,7 +3,6 @@
   <span class="byline author vcard">
   {{ __('by', 'sage') }} <a href="{{ get_author_posts_url(get_the_author_meta('ID')) }}" rel="author" class="fn">
     {{ get_the_author() }}
-      {!! App\app::category() !!}
   </a>
 </span>
 </div>
