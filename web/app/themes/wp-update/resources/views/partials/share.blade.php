c6656d32c803021e549399e0c2a5a78053de58b0
kawax <kawaxbiz@gmail.com>
2018-03-18 12:55:19 +0900

    delete share

---
+++ web/app/themes/wp-update/resources/views/partials/share.blade.php
@@ -0,0 +1,23 @@
+<div class="mt-4">
+  <a href="https://twitter.com/share"
+     class="twitter-share-button"
+     data-hashtags="WordPress"
+     data-show-count="false">Tweet</a>
+  <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+  <a href="http://b.hatena.ne.jp/entry/"
+     class="hatena-bookmark-button"
+     data-hatena-bookmark-layout="basic-label-counter"
+     data-hatena-bookmark-lang="ja"
+     title="このエントリーをはてなブックマークに追加">
+    <img src="https://b.st-hatena.com/images/entry-button/button-only@2x.png"
+         alt="このエントリーをはてなブックマークに追加"
+         width="20"
+         height="20"
+         style="border: none;"/>
+  </a>
+  <script type="text/javascript"
+          src="https://b.st-hatena.com/js/bookmark_button.js"
+          charset="utf-8"
+          async="async"></script>
+</div>
