e51f327df1595437655111b23ccb27449d24cb5b
kawax <kawaxbiz@gmail.com>
2018-05-03 15:25:26 +0900

    head

--- web/app/themes/wp-update/resources/views/partials/head.blade.php
+++ web/app/themes/wp-update/resources/views/partials/head.blade.php
@@ -1,27 +1,31 @@
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-@php wp_head() @endphp
+    @php wp_head() @endphp
+
+    @if(is_home())
+        <meta name="description" content="{{ get_bloginfo('description') }}"/>
+    @endif
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113865733-1"></script>
-  <script>
-    window.dataLayer = window.dataLayer || [];
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-113865733-1"></script>
+    <script>
+      window.dataLayer = window.dataLayer || []
 
-    function gtag() {
-      dataLayer.push(arguments);
-    }
+      function gtag () {
+        dataLayer.push(arguments)
+      }
 
-    gtag('js', new Date());
+      gtag('js', new Date())
 
-    gtag('config', 'UA-113865733-1');
-  </script>
+      gtag('config', 'UA-113865733-1')
+    </script>
 
-  <meta name="google-site-verification" content="gCXeXeRUe2AYiVELS5a4wVWY-TujX9z1-rs2dnIRBy0"/>
+    <meta name="google-site-verification" content="gCXeXeRUe2AYiVELS5a4wVWY-TujX9z1-rs2dnIRBy0"/>
 
-  <link rel="manifest" href="/manifest.json">
-  <meta name="theme-color" content="#0073aa"/>
+    <link rel="manifest" href="/manifest.json">
+    <meta name="theme-color" content="#0073aa"/>
 
 </head>
