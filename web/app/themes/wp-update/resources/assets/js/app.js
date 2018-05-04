8ab3a6997fcdd32abae5362090f9bbc4611ab40a
kawax <kawaxbiz@gmail.com>
2018-05-04 12:59:47 +0900

    sw

--- web/app/themes/wp-update/resources/assets/js/app.js
+++ web/app/themes/wp-update/resources/assets/js/app.js
@@ -5,14 +5,4 @@ import Barba from 'barba.js'
 Barba.Pjax.start()
 Barba.Prefetch.init()
 
-window.addEventListener('load', () => {
-  if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').
-      then((reg) => {
-        console.log('ServiceWorker registered')
-      }).
-      catch((error) => {
-        console.warn('ServiceWorker error', error)
-      })
-  }
-})
+require('./sw')
