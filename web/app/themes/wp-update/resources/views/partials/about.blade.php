f84eed8c24ae95aaf31b040ce6bb6e7cafbba531
kawax <kawaxbiz@gmail.com>
2018-11-26 17:43:45 +0900

    about

--- web/app/themes/wp-update/resources/views/partials/about.blade.php
+++ web/app/themes/wp-update/resources/views/partials/about.blade.php
@@ -1,82 +1,88 @@
 <div class="card mb-3">
-    <div class="card-body">
-        <h2>About</h2>
+  <div class="card-body">
+    <h2>About</h2>
 
-        <p class="lead">
-            古いバージョンのまま放置されてるWordPressを減らしたい。
-        </p>
+    <p class="lead">
+      古いバージョンのまま放置されてるWordPressを減らしたい。
+    </p>
 
-        <h3>最新バージョンを使う理由</h3>
-        <p>なによりもセキュリティ。WordPressの脆弱性がニュースになりますが今のWordPressは自動更新できるのできちんと設定しておけばほとんどの場合は問題ありません。</p>
-        <p>PHPのアップデートも必要。すでに5.5以下はサポート終了。5.6と7.0も2018年末で終了。1年ごとに新バージョンが出て3年前のバージョンがサポート終了するリリーススケジュール。</p>
-        <p>WordPressに必要なPHPバージョンも徐々に上げていく計画が始まっているので今後はWordPressもPHPもアップデートしていく必要があります。
-            <a href="https://make.wordpress.org/core/2018/01/09/servehappy-roadmap/"
-               target="_blank"
-               rel="noopener">servehappy</a></p>
+    <h3>最新バージョンを使う理由</h3>
+    <p>なによりもセキュリティ。WordPressの脆弱性がニュースになりますが今のWordPressは自動更新できるのできちんと設定しておけばほとんどの場合は問題ありません。</p>
+    <p>PHPのアップデートも必要。すでに5.5以下はサポート終了。5.6と7.0も2018年末で終了。1年ごとに新バージョンが出て3年前のバージョンがサポート終了するリリーススケジュール。</p>
+    <p>WordPressに必要なPHPバージョンも徐々に上げていく計画が始まっているので今後はWordPressもPHPもアップデートしていく必要があります。
+      <a href="https://make.wordpress.org/core/2018/01/09/servehappy-roadmap/"
+         target="_blank"
+         rel="noopener">servehappy</a></p>
 
-        <h3>Discord</h3>
-        <ul>
-            <li>
-                <a href="https://discord.gg/2PqBKVd"
-                   target="_blank" rel="noopener">招待リンク</a>から誰でも参加できます。
-            </li>
-        </ul>
+    <h3>Discord</h3>
+    <ul>
+      <li>
+        <a href="https://discord.gg/2PqBKVd"
+           target="_blank" rel="noopener">招待リンク</a>から誰でも参加できます。
+      </li>
+    </ul>
 
-        <h3>募集中</h3>
-        <ul>
-            <li>WordPressのアップデートと運用ができる参加者：法人でも個人でも。個人の場合はチームを組んで運営も可。</li>
-            <li>
-                ここのaboutの文章：<a href="https://github.com/kawax/wp-update.info" target="_blank" rel="noopener">GitHub</a>で。
-            </li>
-            <li>このサイトの運営会社。作業もサーバー費用出す必要もなく名前だけの運営会社になってもいい会社があればSlackから連絡を。</li>
-        </ul>
+    <h3>募集中</h3>
+    <ul>
+      <li>WordPressのアップデートと運用ができる参加者：法人でも個人でも。個人の場合はチームを組んで運営も可。</li>
+      <li>
+        ここのaboutの文章：<a href="https://github.com/kawax/wp-update.info" target="_blank" rel="noopener">GitHub</a>で。
+      </li>
+      <li>このサイトの運営会社。作業もサーバー費用出す必要もなく名前だけの運営会社になってもいい会社があればSlackから連絡を。</li>
+    </ul>
 
-        <h3>参加条件</h3>
-        <ul>
-            <li>古いWordPressはおそらくサーバーも古いので新しいサーバーに移行作業ができる。</li>
-            <li>その後のアップデートも続けられる。</li>
-            <li>適切な料金を設定できる。異常な安売りがこの状況を招いてる。</li>
-        </ul>
+    <h3>参加条件</h3>
+    <ul>
+      <li>古いWordPressはおそらくサーバーも古いので新しいサーバーに移行作業ができる。</li>
+      <li>その後のアップデートも続けられる。</li>
+      <li>適切な料金を設定できる。異常な安売りがこの状況を招いてる。</li>
+    </ul>
 
-        <h3>参加方法</h3>
-        <ul>
-            <li>
-                WordPressのユーザー機能を使っているので普通に
-                <a href="{{ home_url('/login/?action=register') }}"
-                   class="btn btn-outline-dark"
-                   rel="nofollow">
-                    ユーザー登録
-                </a>
-                後プロフィールを設定。
-            </li>
-            <li>参加者リストに表示を有効にするまでは表示されない。使用するのは表示名と下のほうの追加項目。</li>
-            <li>WordPressの権限としては「寄稿者」なので投稿可能。</li>
-            <li>退会機能と二要素認証対応。</li>
-        </ul>
+    <h3>参加方法</h3>
+    <ul>
+      <li>
+        WordPressのユーザー機能を使っているので普通に
+        <a href="{{ home_url('/login/?action=register') }}"
+           class="btn btn-outline-dark"
+           rel="nofollow">
+          ユーザー登録
+        </a>
+        後プロフィールを設定。
+      </li>
+      <li>参加者リストに表示を有効にするまでは表示されない。使用するのは表示名と下のほうの追加項目。</li>
+      <li>WordPressの権限としては「寄稿者」なので投稿可能。</li>
+      <li>退会機能と二要素認証対応。</li>
+    </ul>
 
-        <h3>アップデートの申し込み方法</h3>
-        <ul>
-            <li>対象は「制作会社に任せてたので分からない」「更新して動かなくなるのが怖い」「更新できないように設定されている」ような人です。</li>
-            <li>制作会社のサポートを受けているなら更新するように問い合わせましょう。更新もやってくれないような会社なら切り捨てて他の会社を探しましょう。</li>
-            <li>各参加者のページから申し込み方法を確認して直接申し込んでください。</li>
-        </ul>
+    <h3>アップデートの申し込み方法</h3>
+    <ul>
+      <li>対象は「制作会社に任せてたので分からない」「更新して動かなくなるのが怖い」「更新できないように設定されている」ような人です。</li>
+      <li>制作会社のサポートを受けているなら更新するように問い合わせましょう。更新もやってくれないような会社なら切り捨てて他の会社を探しましょう。</li>
+      <li>各参加者のページから申し込み方法を確認して直接申し込んでください。</li>
+    </ul>
 
-        <h3>リンク</h3>
-        <ul>
-            <li>
-                <a href="https://medinathoughts.com/2018/01/29/wordpress-google/"
-                   target="_blank" rel="noopener">
-                    WordPress @ Google | Alberto Medina&apos;s Blog
-                </a>
-                GoogleがWordPressの改善に力を入れ始めている。古いバージョンのままでは恩恵を受けられない。
-            </li>
-            <li>
-                <a href="https://www.ipa.go.jp/security/ciadr/vul/20170206-wordpress.html"
-                   target="_blank" rel="noopener">
-                    WordPress の脆弱性対策について：IPA 独立行政法人 情報処理推進機構
-                </a>
-                一番最近の影響の大きかった脆弱性。
-            </li>
-        </ul>
-    </div>
+    <h3>リンク</h3>
+    <ul>
+      <li>
+        <a href="https://medinathoughts.com/2018/01/29/wordpress-google/"
+           target="_blank" rel="noopener">
+          WordPress @ Google | Alberto Medina&apos;s Blog
+        </a>
+        GoogleがWordPressの改善に力を入れ始めている。古いバージョンのままでは恩恵を受けられない。
+      </li>
+      <li>
+        <a href="https://www.ipa.go.jp/security/ciadr/vul/20170206-wordpress.html"
+           target="_blank" rel="noopener">
+          WordPress の脆弱性対策について：IPA 独立行政法人 情報処理推進機構
+        </a>
+        一番最近の影響の大きかった脆弱性。
+      </li>
+      <li>
+        <a href="https://internet.watch.impress.co.jp/docs/event/1154306.html"
+           target="_blank" rel="noopener">
+          「注意喚起しても効かない……」、さくらインターネットから利用者への“お願い”【JPAAWG 1st General Meeting】 - INTERNET Watch
+        </a>
+      </li>
+    </ul>
+  </div>
 </div>
