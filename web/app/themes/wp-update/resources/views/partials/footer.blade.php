57c5524ac055d808f9130931dc5a570f6a0e07f2
kawax <kawaxbiz@gmail.com>
2018-11-29 10:51:56 +0900

    cu

--- web/app/themes/wp-update/resources/views/partials/footer.blade.php
+++ web/app/themes/wp-update/resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="content-info">
     <div class="container-fluid p-3 text-center text-white bg-dark">
-        運営会社
+        運営会社募集中
 
         @php dynamic_sidebar('sidebar-footer') @endphp
     </div>
